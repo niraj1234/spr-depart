377d7917740384266794662d183729bfe7572120
Niraj <niraj.webtech@gmail.com>
2023-01-17 04:11:11 +0530

    Just Alignment

--- src/main/java/com/niraj/repository/DepartmentRepository.java
+++ src/main/java/com/niraj/repository/DepartmentRepository.java
@@ -1,7 +1,10 @@
 package com.niraj.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.niraj.entity.Department;
@@ -15,4 +18,8 @@ public interface DepartmentRepository extends JpaRepository<Department , Long>{
 	@Query(value = "select * from department where department_code = ?1 " , nativeQuery = true )
 	public Department getDepartmentByCode(String deptCode);
 
+	@Query(nativeQuery = true , value = "select * from department where department_address = :addr " )
+	public List<Department> getDepartmentByAddress(@Param("addr") String addr);
+
+	
 }
